130202b8e45c9375b5282ec99bae3f310b799606
shih-ting-lin <107251983+shih-ting-lin@users.noreply.github.com>
2022-09-11 01:09:43 +0800

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,21 @@
+var title = document.getElementById("title");
+var content = document.getElementById("content");
+var btn = document.getElementById("btn");
+var list = document.getElementById("list");
+
+btn.addEventListener("click", function(){
+    if(title.value=="" || content.value==""){
+        alert("時間與待辦事項為必填");
+    }
+    else{
+    list.innerHTML = list.innerHTML + `
+    <div class="article">
+        <h2>${title.value}</h2> 
+        <p>${content.value}</p>
+    </div>
+    `;
+    title.value = "";
+    content.value = ""
+    }
+})
+ 
